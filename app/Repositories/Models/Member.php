736fefa89a56f9d541860f7d20c245186e5d73dd
Jefferson Frade <jefferrson.frade@gmail.com>
2020-06-09 15:00:18 -0300

    Finished Members part

--- app/Repositories/Models/Member.php
+++ app/Repositories/Models/Member.php
@@ -5,17 +5,16 @@ namespace App\Repositories\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Disk extends Model
+class Member extends Model
 {
     use SoftDeletes;
 
     protected $connection = 'mysql';
-    protected $table = 'disks';
-
+    protected $table = 'members';
     protected $fillable = [
         'name',
-        'year',
-        'record_company',
-        'cover'
+        'birth',
+        'description',
+        'photo'
     ];
 }
---
+++ database/migrations/2020_06_09_145434_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->date('birth');
+            $table->string('description', 5000);
+            $table->string('photo', 5000);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
